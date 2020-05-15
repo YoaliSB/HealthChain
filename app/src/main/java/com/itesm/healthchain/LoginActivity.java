8f5e1ee8d32bccf306c01520f84d5af4560c12b9
Yoali <sb.yoali@gmail.com>
2020-05-14 23:04:39 -0500

    move login to repository/data source

--- app/src/main/java/com/itesm/healthchain/LoginActivity.java
+++ app/src/main/java/com/itesm/healthchain/LoginActivity.java
@@ -1,129 +1,52 @@
 package com.itesm.healthchain;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import com.itesm.healthchain.data.LoginDataSource;
+import com.itesm.healthchain.data.UserRepository;
+import com.itesm.healthchain.data.model.LoggedInUser;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class LoginActivity extends AppCompatActivity {
-    private static final String url = "https://health-chain-api.herokuapp.com/api/login";
-    EditText editemail, editpass;
+import androidx.appcompat.app.AppCompatActivity;
+public class LoginActivity extends AppCompatActivity implements UserRepository.LoginStateListener {
+    EditText editEmail, editPass;
     Button btnLogin;
     TextView tv;
+    UserRepository repository;
+    LoginDataSource dataSource;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        dataSource = new LoginDataSource();
+        repository = UserRepository.getInstance(dataSource, this);
+        repository.setLoginListener(this);
         tv = findViewById(R.id.textSignUp);
-        editemail = findViewById(R.id.etemail);
-        editpass = findViewById(R.id.etpass);
+        editEmail = findViewById(R.id.etemail);
+        editPass = findViewById(R.id.etpass);
         btnLogin = findViewById(R.id.btnlogin);
         final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String email = editemail.getText().toString().trim();
-                final String pass = editpass.getText().toString().trim();
-
-                JSONObject jsonBody = new JSONObject();
-                if(TextUtils.isEmpty(email) || !email.matches(emailPattern))
-                {
-                    editemail.setError("Ingresa un correo valido");
+                final String email = editEmail.getText().toString().trim();
+                final String pass = editPass.getText().toString().trim();
+                if(TextUtils.isEmpty(email) || !email.matches(emailPattern)) {
+                    editEmail.setError("Ingresa un correo valido");
                     return;
                 }
-                if(TextUtils.isEmpty(pass) || pass.length() < 6)
-                {
-                    editpass.setError("Contraseña incorrecta");
+                if(TextUtils.isEmpty(pass) || pass.length() < 6) {
+                    editPass.setError("Contraseña incorrecta");
                     return;
                 }
-
-                try {
-                    jsonBody.put("email", email);
-                    jsonBody.put("password", pass);
-
-                    JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.POST, url,
-                            jsonBody, new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            Log.i("respuesta", response.toString());
-                            try {
-                                String role = response.getJSONObject("data")
-                                        .getJSONArray("roles")
-                                        .getJSONObject(0)
-                                        .getString("name");
-                                login(role);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(), getString(R.string.login_error),
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    }) {
-                        @Override
-                        protected Map<String, String> getParams(){
-                            Map<String, String> params = new HashMap<>();
-                            params.put("email", email);
-                            params.put("password", pass);
-                            return params;
-                        }
-
-                        @Override
-                        public Map<String, String> getHeaders() throws AuthFailureError {
-                            final Map<String, String> headers = new HashMap<>();
-                            headers.put("Content-Type", "application/json");
-                            return headers;
-                        }
-                    };
-                    RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
-                    requestQueue.add(jsonObject);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-
-                //METODO GET Este si funciona
-                /*JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                        (Request.Method.GET, LG, null, new Response.Listener<JSONObject>() {
-
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Log.i("Response: ", response.toString());
-                            }
-                        }, new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // TODO: Handle error
-
-                            }
-                        });*/
+                repository.login(email, pass, getApplicationContext());
             }
         });
 
@@ -133,10 +56,9 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
-
     }
 
-    private void login(String role) {
+    private void redirect(String role) {
         switch (role) {
             case "user":
                 startActivity(new Intent(LoginActivity.this,
@@ -153,6 +75,10 @@ public class LoginActivity extends AppCompatActivity {
             default:
                 Toast.makeText(getApplicationContext(), "whoops", Toast.LENGTH_LONG).show();
         }
+    }
 
+    @Override
+    public void onLoginSuccess(LoggedInUser user) {
+        redirect(user.getRole());
     }
 }
