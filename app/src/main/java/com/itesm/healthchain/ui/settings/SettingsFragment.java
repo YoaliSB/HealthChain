833d3941bce136c123f74091eba3748a0d57f99f
Kenyiro Tsuru <kenyiro.tsuru@gmail.com>
2020-05-16 21:45:28 -0500

    logout en settings fragment pero sin probar pq necesito nfc

--- app/src/main/java/com/itesm/healthchain/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/itesm/healthchain/ui/settings/SettingsFragment.java
@@ -1,10 +1,12 @@
 package com.itesm.healthchain.ui.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.itesm.healthchain.LoginActivity;
 import com.itesm.healthchain.R;
 
 import androidx.annotation.NonNull;
@@ -18,8 +20,16 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         View root = inflater.inflate(R.layout.settings_fragment, container, false);
         View aboutBtn = root.findViewById(R.id.btn_info);
         View editBtn = root.findViewById(R.id.btn_edit_profile);
+        View logOutBtn = root.findViewById(R.id.btn_logout);
+
         aboutBtn.setOnClickListener(this);
         editBtn.setOnClickListener(this);
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
         return root;
     }
 
