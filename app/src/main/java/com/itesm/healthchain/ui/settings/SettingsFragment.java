f0ade0e2d777579c813ef75ae530303b3af3ebd7
Yoali <sb.yoali@gmail.com>
2020-06-11 13:01:02 -0500

    send email

--- app/src/main/java/com/itesm/healthchain/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/itesm/healthchain/ui/settings/SettingsFragment.java
@@ -1,7 +1,10 @@
 package com.itesm.healthchain.ui.settings;
 
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,9 @@ import com.itesm.healthchain.EmailActivity;
 import com.itesm.healthchain.R;
 import com.itesm.healthchain.data.session.LogoutStateListener;
 import com.itesm.healthchain.data.session.UserRepository;
+
+import java.util.List;
+import java.util.Stack;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -42,7 +48,15 @@ public class SettingsFragment extends Fragment implements View.OnClickListener, 
                 replaceFragment(R.id.navigation_account);
                 break;
             case R.id.btn_email:
-                replaceFragment(R.id.navigation_email);
+//
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("*/*");
+                i.putExtra(Intent.EXTRA_EMAIL, new String[] {
+                        "info@healthchain.com"
+                });
+                i.putExtra(Intent.EXTRA_SUBJECT, "HealthChain App: Dudas o comentarios");
+
+                startActivity(createEmailOnlyChooserIntent(i, "Elige una app para mandar el correo."));
                 break;
             case R.id.btn_logout:
                 UserRepository repository = new UserRepository(getActivity());
@@ -64,4 +78,31 @@ public class SettingsFragment extends Fragment implements View.OnClickListener, 
     public void onLogoutFailure() {
         Toast.makeText(getActivity(), R.string.logout_error, Toast.LENGTH_LONG).show();
     }
+
+    public Intent createEmailOnlyChooserIntent(Intent source,
+                                               CharSequence chooserTitle) {
+        Stack<Intent> intents = new Stack<Intent>();
+        Intent i = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",
+                "info@healthchain.com", null));
+        List<ResolveInfo> activities = getActivity().getPackageManager()
+                .queryIntentActivities(i, 0);
+
+        for(ResolveInfo ri : activities) {
+            Intent target = new Intent(source);
+            target.setPackage(ri.activityInfo.packageName);
+            intents.add(target);
+        }
+
+        if(!intents.isEmpty()) {
+            Intent chooserIntent = Intent.createChooser(intents.remove(0),
+                    chooserTitle);
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                    intents.toArray(new Parcelable[intents.size()]));
+
+            return chooserIntent;
+        } else {
+            return Intent.createChooser(source, chooserTitle);
+        }
+    }
+
 }
